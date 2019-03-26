6fd8530461fb52ecabbac8efee2cb9887c47713d
WebCompanyOKA <41568667+WebCompanyOKA@users.noreply.github.com>
2019-03-26 16:17:05 +0200

    Add files via upload

---
+++ mongo.js
@@ -0,0 +1,12 @@
+db.Zakaz.insertOne(
+{
+    _id:"2",
+"name":"Kirill",
+"e-mail":"admin@mail.ru",
+    "age":"33",
+    "FavColors":["Красный","Синий"],
+    "Child":{"name":"ded","age":"3",}
+
+}
+
+)
